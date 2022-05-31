9e8d434af7b5848372d594832c3db2ecf9900e24
NhanPham <annhandt09@gmail.com>
2022-05-31 18:47:32 +0700

    Update modbus.py

--- modbus.py
+++ modbus.py
@@ -10,7 +10,15 @@ class ModbusPull():
         self.msgError = ''
         self.__timerun = 0
         self.__initCom(1)
-        
+
+    @property
+    def msg_error(self):
+        return self.msgError
+
+    @msg_error.setter
+    def msg_error(self, msg):
+        self.msgError = msg
+
     def __initCom(self,timeout = 0.5):
         PORT = self.__getCOMs()
         if self.msgError == 'ok':
@@ -49,22 +57,38 @@ class ModbusPull():
             if DEBUG:print(self.msgError, flush=True)
             return 0
 
+    def retries(func):
+        def check(self,*args, **kwarg):
+            retries = 3
+            for i in range(retries):
+                self.__wait_to_next_time(timeout = 0.025)
+                if DEBUG:print("ID",id,"retry",i, flush=True)
+                try:
+                    dataGet = func(self,*args, **kwarg)
+                    self.__save_time_run()  
+                    return True, dataGet      
+                except Exception:
+                    self.__save_time_run()
+                    continue
+            return False, 0
+        return check
+
+    @retries
     def get_data(self,id,function,address,value,format = None, tries= 3):
-        retries = tries
-        for i in range(retries):
-            self.__wait_to_next_time(timeout = 0.025)
-            try:
-                
-                if format == None:
-                    dataGet = self.master.execute(id, function, address, value)  
-                else:
-                    dataGet = self.master.execute(id, function, address, value,data_format = format)  
-                self.__save_time_run()  
-                return True, dataGet      
-            except Exception:
-                self.__save_time_run()
-                continue
-        return False, 0
+        if format == None:
+            dataGet = self.master.execute(id, function, address, value)  
+        else:
+            dataGet = self.master.execute(id, function, address, value,data_format = format)  
+
+    @retries
+    def send_data(self,id,function,address,value,format = None,tries = 3)->boolean:
+
+        if format == None:
+            self.master.execute(id, function, address, output_value = value)
+        else:
+            self.master.execute(id, function, address, output_value = value,data_format = format)
+
+
 
     def __save_time_run(self):
         self.__timerun = time.time()
@@ -75,64 +99,6 @@ class ModbusPull():
             if DEBUG:print("wait send",timeout - time_switch_frame , flush=True)
             time.sleep(timeout - time_switch_frame)  
 
-    def send_data(self,id,function,address,value,format = None,tries = 3)->boolean:
-        retries = tries
-        for i in range(retries):
-            self.__wait_to_next_time(timeout = 0.025)
-            try:
-                if DEBUG:print("ID",id,"retry",i, flush=True)
-                if format == None:
-                    self.master.execute(id, function, address, output_value = value)
-                else:
-                    self.master.execute(id, function, address, output_value = value,data_format = format)
-                self.stateModbus = 0
-                self.__save_time_run()
-                return True   
-            except Exception:
-                self.__save_time_run()
-                continue
-                
-        self.stateModbus = id
-        return False  
-    """
-    def getData(self,id,function,begin,end,):
-        dataJ = {
-            'sts':0,
-            'dta':0
-        }
-        try:
-            dataJ['dta'] = self.master.execute(id, function, begin, end)          
-        except modbus_tk.modbus.ModbusError as e:
-            dataJ['sts'] = 1
-            dataJ['dta'] = 1
-        except modbus_tk.modbus_rtu.ModbusInvalidResponseError as e:
-            dataJ['sts'] = 2
-            dataJ['dta'] = 2
-        except:
-            dataJ['sts'] = 3
-            dataJ['dta'] = 3
-        return dataJ
-            
-        
-
-    def setData(self,id,function,begin,data):
-        dataJ = {
-            'sts':0,
-            'dta':0
-        }
-        try:
-            dataJ['dta'] = self.master.execute(id, function, begin, output_value = data)
-        except modbus_tk.modbus.ModbusError as e:
-            dataJ['sts'] = 1
-            dataJ['dta'] = 1
-        except modbus_tk.modbus_rtu.ModbusInvalidResponseError as e:
-            dataJ['sts'] = 2
-            dataJ['dta'] = 2
-        except:
-            dataJ['sts'] = 3
-            dataJ['dta'] = 3
-        return dataJ
-    """
 class controlPump(ModbusPull):
     def __init__(self):
         super().__init__()
@@ -183,29 +149,3 @@ if __name__ == "__main__":
         if controlPump.check_finish(193,total_time = 5):
             status = True
             time.sleep(5)
-"""
-#supported modbus functions
-RAW = 0
-READ_COILS = 1
-READ_DISCRETE_INPUTS = 2
-READ_HOLDING_REGISTERS = 3
-READ_INPUT_REGISTERS = 4
-WRITE_SINGLE_COIL = 5
-WRITE_SINGLE_REGISTER = 6
-READ_EXCEPTION_STATUS = 7
-DIAGNOSTIC = 8
-REPORT_SLAVE_ID = 17
-WRITE_MULTIPLE_COILS = 15
-WRITE_MULTIPLE_REGISTERS = 16
-READ_FILE_RECORD = 20
-READ_WRITE_MULTIPLE_REGISTERS = 23
-DEVICE_INFO = 43
-
-def main(modbusmaster):
-    print(modbusmaster.getData(1,3,0,3))
-    print(modbusmaster.setData(1,6,4,500)) ##WRITE_SINGLE_REGISTER
-    print(modbusmaster.setData(1,16,4,[100,100,100,100,100,100,100,100])) ##WRITE_MULTIPLE_REGISTERS
-if __name__ == "__main__":
-    modbus = modbuspull()
-    main(modbus)
-"""
